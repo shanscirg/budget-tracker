ed9b2553ee61f123d358cadfed34b63a158f9f78
Shannon Scirghio <shanscirg7@gmail.com>
2020-04-03 13:40:20 -0700

    added service worker file, manifest, webpack, and took everything out of develop directory

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const config = {
+    entry: "./public/index.js",
+    output: {
+      path: __dirname + "/dist",
+      filename: "bundle.js"
+    },
+    mode: "development"
+  };
+  
+  module.exports = config;
+  
