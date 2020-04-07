8a80376d64b23ca85984d188be3cc8f993ebaecd
Shannon Scirghio <shanscirg7@gmail.com>
2020-04-06 22:58:47 -0700

    deleted commented code, wrote readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# budget-tracker
+# Budget Tracker
+
+## Application Purpose
+This application is for users to budget their money by tracking income and expenses, both online and offline.
+
+## Link to Application
+[Budget Tracker](https://budget-tracker01.herokuapp.com/)
+
+## Screenshots of Application
+![ss](/public/ss1.png)
--- public/service-worker.js
+++ public/service-worker.js
@@ -35,26 +35,6 @@ self.addEventListener("activate", event => {
   );
 });
 
-// self.addEventListener("fetch", event => {
-//   if (event.request.url.startsWith(self.location.origin)) {
-//     event.respondWith(
-//       caches.match(event.request).then(cachedResponse => {
-//         if (cachedResponse) {
-//           return cachedResponse;
-//         }
-
-//         return caches.open(RUNTIME).then(cache => {
-//           return fetch(event.request).then(response => {
-//             return cache.put(event.request, response.clone()).then(() => {
-//               return response;
-//             });
-//           });
-//         });
-//       })
-//     );
-//   }
-// });
-
 self.addEventListener("fetch", event => {
   // non GET requests are not cached and requests to other origins are not cached
   if (
